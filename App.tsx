d8dc271216a1887a7a8a0a35af35cc4b5d930174
Pendelait <78800759+Pendelait@users.noreply.github.com>
2022-10-20 14:10:25 +0200

    Create Single Page Screen model

--- App.tsx
+++ App.tsx
@@ -4,14 +4,27 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 import ContactScreen from "./src/screens/ContactScreen";
-
+import SinglePageScreen from "./src/screens/SinglePageScreen"
+import { Header } from 'react-native/Libraries/NewAppScreen';
 const Stack = createNativeStackNavigator();
 
+import * as React from 'react';
+import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
+
+
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Sail': require('./assets/Fonts/Sail-Regular.otf'),
+    'Noteworthy-Lt' : require('./assets/Fonts/Noteworthy-Lt.ttf'),
+    'Noteworthy-Bold' : require('./assets/Fonts/Noteworthy-Bold.ttf')
+  });
+
   return (
       <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="home" component={HomeScreen} />
+        <Stack.Navigator screenOptions={{headerShown:false}}>
+            <Stack.Screen name="home" component={HomeScreen}/>
             <Stack.Screen name="contact" component={ContactScreen} />
         </Stack.Navigator>
       </NavigationContainer>
