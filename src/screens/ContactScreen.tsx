e668bab05cb8cf26d431df2aeef4d0188c4865b6
adilrouichi <arouichi@eudonet.com>
2022-10-21 09:02:52 +0200

    fix background and font

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -1,31 +1,31 @@
-import { Text, View, ImageBackground, StyleSheet, Image, ScrollView, SafeAreaView} from "react-native";
+import { Text, View, ImageBackground, StyleSheet, Image, ScrollView, SafeAreaView } from "react-native";
 import Button from "../component/button";
 import Header from '../component/header';
-import {FontAwesome} from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
-    
 
-export default function ContactScreen({navigation} : {navigation : any}){
 
-    return(
-    
-        <SafeAreaView style={{flex:1}}>
-        <ImageBackground source={require('../../assets/image/home-background.png')} style={styles.bgImage}>
+export default function ContactScreen({ navigation }: { navigation: any }) {
+
+  return (
+
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={require('../../assets/image/background.png')} style={styles.bgImage}>
         <Header />
-        <View style={{flex:1}}>
-        
-        <ScrollView>
-            <View style={{ padding:20}}>
-                <Text style={styles.titleText}>Contacts</Text>
+        <View style={{ flex: 1 }}>
+
+          <ScrollView>
+            <View style={{ padding: 20 }}>
+              <Text style={styles.titleText}>Contacts</Text>
             </View>
             <View>
               <Image source={require('../../assets/image/thibault.jpg')} style={styles.image}></Image>
             </View>
-            <View style={{flex:4}}>
+            <View style={{ flex: 4 }}>
               <Text style={styles.text1}>
                 {"\n"}06.63.99.99.78
-                {"\n"}lebateaudethibault@gmail.com  
+                {"\n"}lebateaudethibault@gmail.com
                 {"\n"}www.facebook.com/lebateaudethibault
               </Text>
               <Text style={styles.text2}>
@@ -40,46 +40,44 @@ export default function ContactScreen({navigation} : {navigation : any}){
                 {"\n"}Que du bonheur!
                 {"\n"}Que du bonheur!
               </Text>
-            </View>   
-            </ScrollView>       
+            </View>
+          </ScrollView>
         </View>
-        </ImageBackground>
-        </SafeAreaView>   
-    )
+      </ImageBackground>
+    </SafeAreaView>
+  )
 
-    // add a background image to the home screen
-
-
+  // add a background image to the home screen
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
 
-    titleText:{
-        textAlign : "center",
-        fontSize : 30,
-        fontFamily: 'Sail'
-    },
-    text1:{
-        textAlign : "center",
-        fontSize : 20,
-        fontFamily: 'Sail'
-    },
-    text2:{
-      textAlign : "center",
-      fontSize : 15,
-      padding : 20,
-      fontFamily: 'Sail'
-    },
-    image:{
-        width : "100%",
-        height : 200,
-        resizeMode : 'contain'
-        
-    },
-    bgImage:{
-        width : "100%",
-        height : "100%",
-        resizeMode : 'cover'
-        
-    },
+  titleText: {
+    textAlign: "center",
+    fontSize: 30,
+    fontFamily: 'Sail',
+    color: "white"
+  },
+  text1: {
+    textAlign: "center",
+    fontSize: 20
+  },
+  text2: {
+    textAlign: "center",
+    fontSize: 15,
+    padding: 20,
+    fontFamily: 'Sail'
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    resizeMode: 'contain'
+
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: 'cover'
+
+  },
 })
